95cd7dab9e3944611968fb828e56a0a5cd7d8e10
David Hide <d.hide@live.co.uk>
2017-09-02 12:32:24 +0100

    Added character counting for tweets and tweaked style.

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,18 @@
 $( function() {
-    var word_count = 0;
+    $("#submit").prop('disabled', true);
     $( "#vocabulary, #tweet-composer" ).sortable({
         connectWith: ".connectedSortable",
-        revert: true
+        revert: true,
+        update: function(e,ui) {
+            if (this === ui.item.parent()[0]) {
+                letter_count();
+            }
+        }
     }).disableSelection();
-    word_count += $(this).text().length;
-    console.log(word_count);
 
+    $("#tweet-composer").trigger("sortupdate"); // trigger the update from sortable
 
-    $("#submit").click(function() {
+    $( "#submit" ).click(function() {
         $( "#tweet-composer li" ).each(function() {
             $("#final-tweet").append($(this).text() + " " );
         });
@@ -16,4 +20,18 @@ $( function() {
         //$('form#submit-tweet').submit();
     });
 
+    function letter_count() {
+        var char_count = 140;
+        $( "#tweet-composer li" ).each(function() {
+            char_count -= $(this).text().length;
+        });
+        $("#characters").text(char_count);
+        if (char_count < 0) {
+            $("#submit").prop('disabled', true);
+            $("#characters").css("color", "red");
+        } else {
+            $("#submit").prop('disabled', false);
+            $("#characters").css("color", "black");
+        }
+    }
 } );
